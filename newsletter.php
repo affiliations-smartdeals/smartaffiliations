9d9f1e29f5458c3a337ec1c65a0279a0ef114151
affiliation-smartdeals <mohamedbenmoussapay@gmail.com>
2025-07-04 09:20:41 +0200

    Update newsletter.php

--- newsletter.php
+++ newsletter.php
@@ -7,6 +7,7 @@ function afficher_formulaire() {
     <!DOCTYPE html>
     <html lang="fr">
     <head>
+    <meta name="robots" content="noindex, nofollow">
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Inscription Newsletter | Smartaffiliations</title>
